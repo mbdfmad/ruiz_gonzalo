65075f4742874b150f9ecb43dbec9e1f4493114c
RuizEspinarGonzalo <gonzaloruizespinar@gmail.com>
2021-09-13 09:35:13 +0200

    13 sep

--- Ejercicios_Práctica0.R
+++ Ejercicios_Práctica0.R
@@ -1,1 +1,20 @@
+library(tidyverse)
+#Usando la función sample
 dado_honesto=sample(1:6, size = 100, replace = TRUE)
+
+#Variable discreta, tabla de frecuencia absoluta
+table(dado_honesto)
+
+dado <-
+  data.frame(A = 1:100, dado_A = dado_honesto)
+
+dado %>% count(dado$dado_A)
+
+#Tabla de frecuencia relativa
+signif(prop.table(table(dado$dado_A)), 2)
+
+dado %>%
+  count(dado_A) %>%
+  mutate(dado_A, relFreq = prop.table(n), n=NULL)
+
+
