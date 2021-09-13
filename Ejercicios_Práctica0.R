c007c55093ada3530c9dd06b00907ab4d0e19912
RuizEspinarGonzalo <gonzaloruizespinar@gmail.com>
2021-09-13 10:26:57 +0200

    d

--- Ejercicios_Práctica0.R
+++ Ejercicios_Práctica0.R
@@ -17,4 +17,16 @@ dado %>%
   count(dado_A) %>%
   mutate(dado_A, relFreq = prop.table(n), n=NULL)
 
+#Dado cargado
+pesos=c(1/7, 1/7, 1/7, 1/7, 1/7, 2/7)
+dado_honesto=sample(1:6, size = 100, replace = TRUE, prob=pesos)
+
+#Uso de rep y seq
+v1=rep(4:1,each=4)
+v2=rep(1:5,times=seq(1,5))
+v3=rep(1:4,4)
+help("select")
+#Tabla mpg2
+library(gapminder)
+gapminder %>% select (starts_with(c))
 
